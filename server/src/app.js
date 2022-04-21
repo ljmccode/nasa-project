99d466f08c942b63a33ce450d77044015ea9c3ae
Laura <lmcsay@gmail.com>
2022-04-20 20:27:38 -0500

    Update middleware order

--- server/src/app.js
+++ server/src/app.js
@@ -9,8 +9,10 @@ const app = express();
 app.use(cors({
   origin: 'http://localhost:3000',
 }));
+
 app.use(express.json());
-app.use(planetsRouter);
 app.use(express.static(path.join(__dirname, '..', 'public')));
+
+app.use(planetsRouter);
 
 module.exports = app;
