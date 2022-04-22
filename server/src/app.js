0b868aabc0b6367ee90868035e31f75fa66b6d0f
Laura <lmcsay@gmail.com>
2022-04-21 22:12:51 -0500

    Add launch router middleware to app

--- server/src/app.js
+++ server/src/app.js
@@ -4,6 +4,7 @@ const cors = require('cors');
 const morgan = require('morgan');
 
 const planetsRouter = require('./routes/planets/planets.router');
+const launchesRouter = require('./routes/launches/launches.router');
 
 const app = express();
 
@@ -16,5 +17,6 @@ app.use(express.json());
 app.use(express.static(path.join(__dirname, '..', 'public')));
 
 app.use(planetsRouter);
+app.use(launchesRouter);
 
-module.exports = app;
+module.exports = app; 
