042b0f9c81c05a7d3437aa9f78dc0298ffff67bd
Laura <lmcsay@gmail.com>
2022-04-22 15:15:27 -0500

    Serve unknown routes to public index.html file

--- server/src/app.js
+++ server/src/app.js
@@ -18,5 +18,8 @@ app.use(express.static(path.join(__dirname, '..', 'public')));
 
 app.use(planetsRouter);
 app.use(launchesRouter);
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, '..', 'public', 'index.html'));
+})
 
 module.exports = app; 
