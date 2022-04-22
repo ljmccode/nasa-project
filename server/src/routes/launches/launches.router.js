1e85c9e3d3d386a8a36dd5ae1ecc836f0a7dd93c
Laura <lmcsay@gmail.com>
2022-04-22 15:20:00 -0500

    Update router to match new controller function name

--- server/src/routes/launches/launches.router.js
+++ server/src/routes/launches/launches.router.js
@@ -1,10 +1,10 @@
 const express = require('express');
 
-const { getAllLaunches } = require('./launches.controller');
+const { httpGetAllLaunches } = require('./launches.controller');
 
 const launchRouter = express.Router();
 
-launchRouter.get('/launches', getAllLaunches);
+launchRouter.get('/launches', httpGetAllLaunches);
 
 module.exports = launchRouter;
 
