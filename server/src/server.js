714fc51a4ebbcc0f38ddb081ff9b339f4ab803dc
Laura <lmcsay@gmail.com>
2022-05-16 19:41:18 -0500

    Use mongoose to connect with database

--- server/src/server.js
+++ server/src/server.js
@@ -1,4 +1,6 @@
 const http = require('http');
+const mongoose = require('mongoose');
+require('dotenv').config();
 
 const app = require('./app');
 
@@ -8,8 +10,18 @@ const PORT = process.env.PORT || 8000;
 
 const server = http.createServer(app);
 
+mongoose.connection.once('open', () => {
+  console.log('MongoDB connection ready!');
+})
+
+mongoose.connection.on('error', (err) => {
+  console.error('err');
+})
+
 async function startServer() {
-  await loadPlanetsData();  
+  await mongoose.connect(process.env.MONGO_URI);
+  await loadPlanetsData(); 
+
   server.listen(PORT, () => {
     console.log(`Listening on port ${PORT}...`);
   });
