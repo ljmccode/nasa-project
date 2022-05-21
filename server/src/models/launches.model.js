9f6bb669d6bdfeb4e1c50daa061f37311c0704fb
Laura <lmcsay@gmail.com>
2022-05-21 17:33:24 -0500

    Create function to return lastest flight number

--- server/src/models/launches.model.js
+++ server/src/models/launches.model.js
@@ -1,8 +1,9 @@
 const launchesDatabase = require('./launches.mongo');
+const planets = require('./planets.mongo');
+
+const DEFAULT_FLIGHT_NUMBER = 100;
 
 const launches = new Map();
-
-let latestFlightNumber = 100;
 
 const launch = {
   flightNumber: 100,
@@ -21,29 +22,54 @@ function existsLaunchWithId(launchId) {
   return launches.has(launchId);
 }
 
+async function getLatestFlightNumber() {
+  const latestLaunch = await launchesDatabase.findOne().sort('-flightNumber');
+
+  if (!latestLaunch) {
+    return DEFAULT_FLIGHT_NUMBER;
+  }
+
+  return latestLaunch.flightNumber;
+}
+
 async function getAllLaunches() {
   return await launchesDatabase
     .find({}, {
-    '_id': 0, '__v': 0
+      _id: 0, __v: 0,
   });
 }
 
 async function saveLaunch(launch) {
-  await launchesDatabase.updateOne({
-    flightNumber: launch.flightNumber,
-  }, launch, {
-    upsert: true
-  })
+  const planet = await planets.findOne({
+    keplerName: launch.target,
+  });
+
+  if (!planet) {
+    throw new Error('No matching planet was found');
+  }
+
+  await launchesDatabase.updateOne(
+    {
+      flightNumber: launch.flightNumber,
+    },
+    launch,
+    {
+      upsert: true,
+    }
+  );
 }
 
 function addNewLaunch(launch) {
-  latestFlightNumber++
-  launches.set(latestFlightNumber, Object.assign(launch, {
-    success: true,
-    upcoming: true,
-    customers: ['ZTM, NASA'],
-    flightNumber: latestFlightNumber
-  }));
+  latestFlightNumber++;
+  launches.set(
+    latestFlightNumber,
+    Object.assign(launch, {
+      success: true,
+      upcoming: true,
+      customers: ['ZTM, NASA'],
+      flightNumber: latestFlightNumber,
+    })
+  );
 }
 
 function abortLaunchById(launchId) {
