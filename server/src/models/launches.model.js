f8f64c80fbdd151bee8bc5183b02cae7aebe0bc8
Laura <lmcsay@gmail.com>
2022-05-22 17:03:19 -0500

    Create a launch object with launch data from SpaceX API

--- server/src/models/launches.model.js
+++ server/src/models/launches.model.js
@@ -43,7 +43,26 @@ async function loadLaunchesData() {
           ]
       }
   });
-  console.log('response: ', response);
+
+  const launchDocs = response.data.docs;
+  for (const launchDoc of launchDocs) {
+    const payloads = launchDoc['payloads'];
+    const customers = payloads.flatMap((payload) => {
+      return payload['customers'];
+    });
+
+    const launch = {
+      flightNumber: launchDoc['flight_number'],
+      mission: launchDoc['name'],
+      rocket: launchDoc['rocket']['name'],
+      launchDate: launchDoc['date_local'],
+      upcoming: launchDoc['upcoming'],
+      success: launchDoc['success'],
+      customers
+    };
+
+    console.log(`${launch.mission}`)
+  }
 }
 
 async function existsLaunchWithId(launchId) {
