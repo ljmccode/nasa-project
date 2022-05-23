6ead23b8901086be8ead2caeeaec1cc9b89e4704
Laura <lmcsay@gmail.com>
2022-05-22 21:07:09 -0500

    Remove console.log

--- server/src/routes/launches/launches.controller.js
+++ server/src/routes/launches/launches.controller.js
@@ -1,7 +1,6 @@
 const { getAllLaunches, scheduleNewLaunch, existsLaunchWithId, abortLaunchById } = require('../../models/launches.model');
 
 async function httpGetAllLaunches(req, res) {
-  console.log('got launches');
   return res.status(200).json(await getAllLaunches());
 }
 
