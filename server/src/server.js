9763fed601d7c0cd069db4edca21466cf80f5772
Laura <lmcsay@gmail.com>
2022-06-16 18:57:36 -0500

    Replace loadLaunchesData function with populateLaunches

--- server/src/server.js
+++ server/src/server.js
@@ -5,7 +5,7 @@ require('dotenv').config();
 const app = require('./app');
 const { mongoConnect } = require('./services/mongo')
 const { loadPlanetsData } = require('./models/planets.model');
-const { loadLaunchesData } = require('./models/launches.model');
+const { populateLaunches } = require('./models/launches.model');
 
 const PORT = process.env.PORT || 8000;
 
@@ -14,7 +14,7 @@ const server = http.createServer(app);
 async function startServer() {
   await mongoConnect();
   await loadPlanetsData(); 
-  await loadLaunchesData();
+  await populateLaunches();
 
   server.listen(PORT, () => {
     console.log(`Listening on port ${PORT}...`);
